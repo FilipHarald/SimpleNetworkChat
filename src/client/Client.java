7af503082468aec6442d6579b6fcb2a212167915
nekosaur <albert@kaaman.nu>
2015-03-05 01:16:07 +0100

    Utökat funktionalitet i Client klassen, lagt till ClientListener interface så att GUI kan uppdateras, bytat namn på Message klassen och lagt till konstruktor

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,9 +1,132 @@
 package client;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.ImageIcon;
+
+import message.Message;
+
 /**
  * 
  * @author Albert och Henrik
  *
  */
-public class Client {
+public class Client extends Thread {
+	private String serverHost;
+	private int serverPort;
 
+	private String userName;
+
+	private Thread messageListener;
+	private ObjectOutputStream outputStream;
+
+	private Set<ClientListener> listeners;
+	
+	public Client(String serverHost, int serverPort, String userName) {
+		this.serverHost = serverHost;
+		this.serverPort = serverPort;
+		this.userName = userName;
+		this.listeners = new HashSet<ClientListener>();
+	
+		this.start();
+	}
+
+	public void addListener(ClientListener listener) {
+		listeners.add(listener);
+	}
+
+	public void sendMessage(Message message) {
+		if (outputStream != null) {
+			try {
+				outputStream.writeObject(message);
+				outputStream.flush();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public void sendMessage(String[] recipients, String text, ImageIcon image) {
+		if (outputStream != null) {
+			try {
+				outputStream.writeObject(new Message(userName, recipients, text, image));
+				outputStream.flush();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+
+		try (Socket socket = new Socket(serverHost, serverPort);
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());) {
+
+			outputStream = oos;
+
+			new MessageListener(ois).start();
+
+			fireConnected();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void fireConnected() {
+		for (ClientListener listener : listeners) {
+			listener.onConnected();
+		}
+	}
+
+	private void fireDisconnected() {
+		for (ClientListener listener : listeners) {
+			listener.onDisconnected();
+		}
+	}
+
+	private void fireClientsUpdated(String[] clients) {
+		for (ClientListener listener : listeners) {
+			listener.onClientsUpdated(clients);
+		}
+	}
+
+	private void fireMessageReceived(Message message) {
+		for (ClientListener listener : listeners) {
+			listener.onMessageReceived(message);
+		}
+	}
+
+	private class MessageListener extends Thread {
+
+		private ObjectInputStream inputStream;
+
+		public MessageListener(ObjectInputStream inputStream) {
+			this.inputStream = inputStream;
+		}
+
+		@Override
+		public void run() {
+
+			// Get handshake response
+
+			fireConnected();
+
+			// Get user list
+
+			fireClientsUpdated(null);
+
+			while (true) {
+
+				// Get incoming messages
+
+				fireMessageReceived(null);
+
+			}
+		}
+
+	}
 }
---
+++ src/message/Message.java
@@ -0,0 +1,22 @@
+package message;
+
+import javax.swing.ImageIcon;
+
+/**
+ * 
+ * @author 
+ *
+ */
+public class Message {
+
+	private long received;
+	private long delivered;
+	private String sender;
+	private String[] recipients;
+	private String text;
+	private ImageIcon image;
+
+	public Message(String sender, String[] recipients, String text, ImageIcon image) {
+
+	}
+}
