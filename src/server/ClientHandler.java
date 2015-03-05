5aafc85e463224637696d67e88461a68250d6ff9
FilipHarald <filip.harald@gmail.com>
2015-03-05 09:06:36 +0100

    la till lite i servern

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -2,22 +2,30 @@ package server;
 
 import java.net.Socket;
 
-public class ClientHandler extends Thread{
+public class ClientHandler extends Thread {
 	private Socket socketIn;
-	
-	
+	private Socket socketOut;
+	private String clientName;
+
 	public ClientHandler(Socket socketIn) {
 		this.socketIn = socketIn;
 	}
 
 	@Override
 	public void run() {
-		//skapa ClientHandlerInput 
+		// skapa ClientHandlerInput
 	}
-	
-	private class ClientHandlerInput extends Thread{
-		//ska lyssna på socketIn
+
+//	public boolean sendToClient(Message message) {
+//		boolean sent = false;
+//		// 
+//		// här ska meddelandet skickas med socketOut
+//		//
+//		return sent;
+//	}
+
+	private class ClientHandlerInput extends Thread {
+		// ska lyssna på socketIn
 	}
-	
 
 }
